7088e6dbbd4bee1bc7a184f21bc9d69196f9ddb8
SAHIL AJANI <119123610+sahilxpatel@users.noreply.github.com>
2026-05-29 09:38:57 +0530

    feat: refine database connection setup

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,14 +2,10 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
-    if (!uri) {
-      throw new Error('MONGO_URI is not defined in the environment variables');
-    }
-    const conn = await mongoose.connect(uri);
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error}`);
+  } catch (error: any) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
