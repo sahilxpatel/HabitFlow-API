0b75dcd9e806d2a687868de4b89b2cdf4a0f3ec3
SAHIL AJANI <119123610+sahilxpatel@users.noreply.github.com>
2026-05-29 09:16:06 +0530

    chore: setup MVC + Service Layer project structure

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from './app';
+import { connectDB } from './config/db';
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
